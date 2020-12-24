cb34ed88136a58fae6417e4311bba164d5808e1f
Walker Denial <WalkerDenial@qq.com>
2020-12-24 23:56:01 +0800

    add note

--- src/com/wd/proxy/NormalProxyTest.java
+++ src/com/wd/proxy/NormalProxyTest.java
@@ -22,20 +22,23 @@ public class NormalProxyTest {
      * 缺点：
      * 1. 增加一个新的操作，需要增加新的接口，违反开闭原则，可扩展性差
      * 2. 虽然不需要知道具体实现细节，但是需要知道具体实现类，造成类的额外负担
+     * 3. 还是需要知道具体实现类，导致类变得复杂
+     *
+     * 思考：能不能通过使用注解进行标记，然后通过反射获取对象实例，这样就能避免消费者知道生产者的信息了呢
      *
      * @param args
      */
     public static void main(String[] args) {
-        // 通过代理购买
+        // 通过代理购买 RiceFood
         IFood food = new NormalFoodProxy(new RiceFood());
         food.buyFood();
-
+        // 通过代理购买 BananaFood
         food = new NormalFoodProxy(new BananaFood());
         food.buyFood();
-
+        // 通过代理购买 CarToy
         IToy toy = new NormalToyProxy(new CarToy());
         toy.bugToy();
-
+        // 通过代理购买 BallToy
         toy = new NormalToyProxy(new BallToy());
         toy.bugToy();
     }

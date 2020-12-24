330f8850929ddba165b0c80b092424d6c5ea6785
Walker Denial <WalkerDenial@qq.com>
2020-12-25 00:29:52 +0800

    get generate proxy class

--- src/com/wd/proxy/DynamicProxyTest.java
+++ src/com/wd/proxy/DynamicProxyTest.java
@@ -5,6 +5,9 @@ import com.wd.proxy.source.food.IFood;
 import com.wd.proxy.source.food.RiceFood;
 import com.wd.proxy.source.toy.CarToy;
 import com.wd.proxy.source.toy.IToy;
+import com.wd.proxy.util.ProxyUtil;
+
+import java.lang.reflect.Method;
 
 public class DynamicProxyTest {
 
@@ -13,14 +16,28 @@ public class DynamicProxyTest {
         DynamicProxy proxy = new DynamicProxy();
 
         // 代理 RiceFood（动态代理类里面没有实现 IFood 接口）
-        proxy.setFactory(new RiceFood());
-        IFood rice = (IFood) proxy.getProxyInstance();
-        rice.buyFood();
+        RiceFood riceFood = new RiceFood();
+        proxy.setFactory(riceFood);
+        IFood riceProxy = (IFood) proxy.getProxyInstance();
+        riceProxy.buyFood();
 
         // 代理 CarToy（动态代理类里面没有实现 IToy 接口）
-        proxy.setFactory(new CarToy());
-        IToy toy = (IToy) proxy.getProxyInstance();
-        toy.bugToy();
+        CarToy carToy = new CarToy();
+        proxy.setFactory(carToy);
+        IToy toyProxy = (IToy) proxy.getProxyInstance();
+        toyProxy.bugToy();
+
+        // 从内存中获取动态代理生成的类
+        ProxyUtil.generateClassFile(riceFood.getClass(), riceProxy.getClass().getSimpleName());
+        ProxyUtil.generateClassFile(carToy.getClass(), toyProxy.getClass().getSimpleName());
+        System.out.println("--------------------");
+        for (Method item : riceProxy.getClass().getDeclaredMethods()) {
+            System.out.println(item.getName());
+        }
+        System.out.println("--------------------");
+        for (Method item : toyProxy.getClass().getDeclaredMethods()) {
+            System.out.println(item.getName());
+        }
     }
 
 }
---
+++ src/com/wd/proxy/util/ProxyUtil.java
@@ -0,0 +1,38 @@
+package com.wd.proxy.util;
+
+import sun.misc.ProxyGenerator;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class ProxyUtil {
+
+    public static void generateClassFile(Class clazz, String proxyName) {
+        byte[] proxyClassFile = ProxyGenerator.generateProxyClass(proxyName, new Class[]{clazz});
+        String paths = clazz.getResource(".").getPath();
+        System.out.println(paths);
+        FileOutputStream fos = null;
+        try {
+            File file = new File(paths + proxyName + ".class");
+            if (!file.getParentFile().exists()) file.getParentFile().mkdirs();
+            if (file.exists()) file.delete();
+            file.createNewFile();
+            fos = new FileOutputStream(file);
+            fos.write(proxyClassFile);
+            fos.flush();
+            System.out.println(file.getAbsolutePath());
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (fos != null) {
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+}

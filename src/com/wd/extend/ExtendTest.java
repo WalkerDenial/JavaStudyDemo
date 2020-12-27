057e378064f0eeed430eed2f67309af4b215b739
Walker Denial <WalkerDenial@qq.com>
2020-12-27 23:21:52 +0800

    add code

--- src/com/wd/extend/ExtendTest.java
+++ src/com/wd/extend/ExtendTest.java
@@ -5,6 +5,10 @@ import com.wd.extend.source.Parent;
 
 public class ExtendTest {
 
+    /**
+     * 由输出结果可知，在创建对象时，先创建父类，然后再创建子类
+     * @param args
+     */
     public static void main(String[] args) {
         Parent parent = new Parent();
         System.out.println("--------------");
